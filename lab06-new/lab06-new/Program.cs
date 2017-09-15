187d09256f9538dc217474077ac4155082a09c39
Paweena27 <59030185@kmitl.ac.th>
2017-09-15 10:55:40 +0700

    W06P06

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -7,6 +7,19 @@ namespace lab06_new
     {
         static void Main(string[] args)
         {
+            int b = add(1, 2, 3, 4, 5);
+            Console.WriteLine("b = {0}", b);
+            Console.WriteLine("1+5+8+4+1+6+6+8 = " + add(1, 5, 8, 4, 1, 6, 6, 8));
+            Console.ReadLine();
+        }
+
+        static int add(params int[] i)
+        {
+            int sum = 0; ;
+            foreach (int j in i)
+                sum += j;
+            return sum;
+
         }
     }
 }
