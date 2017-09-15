411fa54c0af9ffe22bc86cff777eeb42e21c66b2
Paweena27 <59030185@kmitl.ac.th>
2017-09-15 10:21:29 +0700

    W06P01

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;  // for web client
+using System.IO;   // for stream writer
+
 
 namespace lab06_new
 {
@@ -10,6 +9,21 @@ namespace lab06_new
     {
         static void Main(string[] args)
         {
+            static void Main()
+            {
+                // Create web client.
+                WebClient client = new WebClient();
+                // Download string.
+                string value = client.DownloadString("http://www.kmitl.ac.th");
+                // Write values.
+                Console.WriteLine("--- WebClient result ---");
+                Console.WriteLine(value.Length);
+                Console.WriteLine(value);
+                StreamWriter outtxt = new StreamWriter("webload.html");
+                outtxt.Write(value);
+                outtxt.Dispose();
+            }
+
         }
     }
 }
