cee93e32235025cfc8a548ba1cf1ae2ece15daeb
Paweena27 <59030185@kmitl.ac.th>
2017-09-15 11:18:30 +0700

    W06P07

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -7,6 +7,20 @@ namespace lab06_new
     {
         static void Main(string[] args)
         {
+            object[] myObjArray = { 2, 'b', 1000.05d, "Hello World" };
+            useParams(myObjArray);
+            useParams("IE.Tech.", "KMITL", 2017);
+            Console.ReadLine();
         }
+
+        static void useParams(params object[] list)
+        {
+            foreach (object o in list)
+                Console.Write(o + " ");
+            Console.WriteLine();
+        }
+
+
     }
 }
+
