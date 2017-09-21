41ac0e8ddf16071b3ff7aab4f5ef9756f482a7b9
Paweena27 <59030185@kmitl.ac.th>
2017-09-21 18:13:35 +0700

    W06P15

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -7,6 +7,12 @@ namespace lab06_new
     {
         static void Main(string[] args)
         {
+            int myInt = 5;
+            Console.WriteLine(myInt);
+            long myLong = myInt;
+            Console.WriteLine(myLong);
+            Console.WriteLine(myLong + myInt);
+
         }
     }
 }
