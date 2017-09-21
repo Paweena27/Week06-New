b7f003141cae0ad1b88f217c49dffb257d93f893
Paweena27 <59030185@kmitl.ac.th>
2017-09-21 18:15:16 +0700

    W06P16

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -7,6 +7,18 @@ namespace lab06_new
     {
         static void Main(string[] args)
         {
+            double myDouble = 9.9d;
+            Console.WriteLine(myDouble);
+
+            long myLong = (long)myDouble;
+            Console.WriteLine(myLong);
+
+            myDouble = 9e9d;
+            Console.WriteLine(myDouble);
+
+            int myInt = (int)myDouble;
+            Console.WriteLine(myInt);
+            Console.WriteLine(int.MinValue);
         }
     }
 }
