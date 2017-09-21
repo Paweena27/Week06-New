77be5cd979fb5088e5a7a21ef9fc1d197ad8b361
Paweena27 <59030185@kmitl.ac.th>
2017-09-21 17:45:59 +0700

    W06P10

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -5,10 +5,17 @@ namespace lab06_new
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int val=999; // uninitialized variable
+            refMethod(ref val);
+            Console.WriteLine(val);
+            Console.ReadLine();
+        }
 
-          
+        static void refMethod(ref int i)
+        {
+            i = i + 1;
 
         }
     }
