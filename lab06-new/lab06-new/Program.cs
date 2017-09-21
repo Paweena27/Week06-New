34aa21c07492627488758a00598c742ab8a0adce
Paweena27 <59030185@kmitl.ac.th>
2017-09-21 17:29:45 +0700

    W06P09

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -7,6 +7,19 @@ namespace lab06_new
     {
         static void Main(string[] args)
         {
+
+
+            int val = 999;
+            refMethod(ref val);
+            Console.WriteLine(val);
+            Console.ReadLine();
+        }
+
+        static void refMethod(ref int i)
+        {
+            i = i + 1;
+
+
         }
     }
 }
