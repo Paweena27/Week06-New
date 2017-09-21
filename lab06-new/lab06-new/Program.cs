751e72b75419540554c774ac8638acee1f1d151d
Paweena27 <59030185@kmitl.ac.th>
2017-09-21 18:02:40 +0700

    W06P13

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -7,6 +7,18 @@ namespace lab06_new
     {
         static void Main(string[] args)
         {
+
+            byte a = 3;
+            byte b = 5;
+
+            Console.WriteLine(a | b);
+            Console.WriteLine(a & b);
+            Console.WriteLine(a ^ b);
+            Console.WriteLine(~a & b);
+            Console.WriteLine(a << 1);
+            Console.WriteLine(a << 2);
+            Console.WriteLine(a >> 1);
+
         }
     }
 }
