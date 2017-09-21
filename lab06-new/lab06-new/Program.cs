3d046e539872c837365c7e80d83f9d5116b4b191
Paweena27 <59030185@kmitl.ac.th>
2017-09-21 20:17:47 +0700

    W06P23

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -4,9 +4,26 @@
 namespace lab06_new
 {
     class Program
+
     {
+        static void trapezoid(double E, double F, double G)
+        {
+            double H;
+            H = (0.5 * (E + F) * G);
+            Console.Write("Area of the trapzoid is : {0}", H); 
+        }
+
         static void Main(string[] args)
         {
+            double E, F, G;
+            Console.Write("Enter Parallel 1");
+            E = double.Parse(Console.ReadLine());
+            Console.Write("Enter Parallel 2");
+            F = double.Parse(Console.ReadLine());
+            Console.Write("Enter high");
+            G = double.Parse(Console.ReadLine());
+            trapezoid(E, F, G);
+            Console.ReadLine();
         }
     }
 }
