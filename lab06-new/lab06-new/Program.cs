4c8a5fa89617112522803436c71a806dcdfd520d
Paweena27 <59030185@kmitl.ac.th>
2017-09-21 19:10:38 +0700

    W06P22

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -5,8 +5,22 @@ namespace lab06_new
 {
     class Program
     {
-        static void Main(string[] args)
+        static void EvenOddNumber(double A)
         {
+            string x;
+            if ((A % 2) > 0)
+            {
+                x = ("This is Odd Number");
+            }
+            else
+            {
+                x = ("This is Even Number");
+            }
+            Console.WriteLine("{0}", x);
+            
         }
+
+      
+            
     }
 }
