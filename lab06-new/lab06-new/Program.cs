03f67555840e38fcd7eaa1d6308bb09c7a0f7ab1
Paweena27 <59030185@kmitl.ac.th>
2017-09-21 20:35:33 +0700

    W06P24

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -5,8 +5,21 @@ namespace lab06_new
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Moon(double X)
         {
+            double W;
+            W = (X * 1.6);
+            Console.WriteLine("Weight on moon is :{0} N.", W);
+
         }
+        static void Main(string[]args)
+        {
+                double X;
+            Console.Write("Enter the Mass (kg)");
+            X = double.Parse(Console.ReadLine());
+            Moon(X);
+            Console.ReadLine();
+        }
+
     }
 }
