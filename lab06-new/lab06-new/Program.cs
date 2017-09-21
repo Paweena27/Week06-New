b6f6f35e4d70f6bd5cd154b802b1a3fa26301b4c
Paweena27 <59030185@kmitl.ac.th>
2017-09-21 18:11:33 +0700

    W06P14

--- lab06-new/lab06-new/Program.cs
+++ lab06-new/lab06-new/Program.cs
@@ -7,6 +7,13 @@ namespace lab06_new
     {
         static void Main(string[] args)
         {
+            string hello = "Hello";
+            string world = "World";
+            string helloWorld = hello + world;
+            Console.WriteLine(helloWorld);
+            string helloWorld555 = helloWorld + " " + 555;
+            Console.WriteLine(helloWorld555);
+
         }
     }
 }
